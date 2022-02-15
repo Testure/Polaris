8b9e38165b1052293acdf958833814d3532b732f
Turing6 <39919441+Testure@users.noreply.github.com>
2022-02-15 05:09:08 -0600

    Add Tags

--- src/main/java/turing/mods/polaris/datagen/ModBlockTagsProvider.java
+++ src/main/java/turing/mods/polaris/datagen/ModBlockTagsProvider.java
@@ -6,7 +6,11 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.TagsProvider;
 import net.minecraft.tags.ITag;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.RegistryObject;
 import turing.mods.polaris.Polaris;
+import turing.mods.polaris.block.SubBlockGenerated;
+import turing.mods.polaris.registry.MaterialRegistry;
+import turing.mods.polaris.registry.MaterialRegistryObject;
 
 import javax.annotation.Nullable;
 
@@ -17,7 +21,23 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags() {
+        addMaterialTags();
+    }
 
+    private void addMaterialTags() {
+        for (MaterialRegistryObject materialRegistryObject : MaterialRegistry.getMaterials().values()) {
+            if (materialRegistryObject.hasBlocks()) {
+                for (RegistryObject<Block> block : materialRegistryObject.getBlocks()) {
+                    if (materialRegistryObject.get().existingItems == null || !materialRegistryObject.get().existingItems.contains(block.get().asItem())) {
+                        SubBlockGenerated blockGenerated = (SubBlockGenerated) block.get();
+                        String parentTag = blockGenerated.getSubItem().name().toLowerCase() + "s";
+
+                        tag(PolarisTags.Blocks.forge(parentTag + "/" + materialRegistryObject.getName())).add(blockGenerated);
+                        tag(PolarisTags.Blocks.forge(parentTag)).add(blockGenerated);
+                    }
+                }
+            }
+        }
     }
 
     public TagsProvider.Builder<Block> getOrCreateBlockBuilder(ITag.INamedTag<Block> tag) {
--- src/main/java/turing/mods/polaris/datagen/ModItemTagsProvider.java
+++ src/main/java/turing/mods/polaris/datagen/ModItemTagsProvider.java
@@ -1,5 +1,6 @@
 package turing.mods.polaris.datagen;
 
+import net.minecraft.block.Block;
 import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.ItemTagsProvider;
@@ -7,8 +8,13 @@ import net.minecraft.data.TagsProvider;
 import net.minecraft.item.Item;
 import net.minecraft.tags.ITag;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.RegistryObject;
 import turing.mods.polaris.Polaris;
+import turing.mods.polaris.block.SubBlockItemGenerated;
+import turing.mods.polaris.item.SubItemGenerated;
 import turing.mods.polaris.registry.ItemRegistry;
+import turing.mods.polaris.registry.MaterialRegistry;
+import turing.mods.polaris.registry.MaterialRegistryObject;
 
 import javax.annotation.Nullable;
 
@@ -19,7 +25,33 @@ public class ModItemTagsProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags() {
+        addMaterialTags();
         tag(PolarisTags.Items.CIRCUIT_LOGIC).add(ItemRegistry.VACUUM_TUBE.get(), ItemRegistry.NAND.get());
+    }
+
+    private void addMaterialTags() {
+        for (MaterialRegistryObject materialRegistryObject : MaterialRegistry.getMaterials().values()) {
+            for (RegistryObject<Item> item : materialRegistryObject.getItems()) {
+                if (materialRegistryObject.get().existingItems == null || !materialRegistryObject.get().existingItems.contains(item.get())) {
+                    SubItemGenerated itemGenerated = (SubItemGenerated) item.get();
+                    String parentTag = itemGenerated.getSubItem().name().toLowerCase() + "s";
+
+                    tag(PolarisTags.Items.forge(parentTag + "/" + materialRegistryObject.getName())).add(itemGenerated);
+                    tag(PolarisTags.Items.forge(parentTag)).add(itemGenerated);
+                }
+            }
+            if (materialRegistryObject.hasBlocks()) {
+                for (RegistryObject<Block> block : materialRegistryObject.getBlocks()) {
+                    if (materialRegistryObject.get().existingItems == null || !materialRegistryObject.get().existingItems.contains(block.get().asItem())) {
+                        SubBlockItemGenerated blockItem = (SubBlockItemGenerated) block.get().asItem();
+                        String parentTag = blockItem.getSubItem().name().toLowerCase() + "s";
+
+                        tag(PolarisTags.Items.forge(parentTag + "/" + materialRegistryObject.getName())).add(blockItem);
+                        tag(PolarisTags.Items.forge(parentTag)).add(blockItem);
+                    }
+                }
+            }
+        }
     }
 
     public TagsProvider.Builder<Item> getOrCreateItemBuilder(ITag.INamedTag<Item> tag) {
