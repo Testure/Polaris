91261a2dc8e1c3bddf5a4a38b0aab09b102e1b54
Turing6 <39919441+Testure@users.noreply.github.com>
2022-02-19 16:15:00 -0600

    SingleItemRecipeIdentifier

--- src/main/java/turing/mods/polaris/recipe/IMachineRecipeIdentifier.java
+++ src/main/java/turing/mods/polaris/recipe/IMachineRecipeIdentifier.java
@@ -1,8 +1,12 @@
 package turing.mods.polaris.recipe;
 
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 /**
  *
@@ -10,5 +14,11 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 public interface IMachineRecipeIdentifier {
+    boolean test(List<IMachineIngredient> ingredients, @Nullable List<FluidStack> fluidInputs, @Nullable List<FluidStack> fluidOutputs, @Nullable List<ItemStack> outputs, @Nullable Integer duration, @Nullable Integer EUt, @Nullable Integer circuit);
 
+    boolean test(List<ItemStack> outputs, @Nullable Integer duration, @Nullable Integer EUt, @Nullable Integer circuit);
+
+    boolean test(List<IMachineIngredient> ingredients, @Nullable Integer EUt, @Nullable Integer circuit);
+
+    boolean test(List<IMachineIngredient> ingredients, List<ItemStack> outputs, @Nullable Integer duration, @Nullable Integer EUt, @Nullable Integer circuit);
 }
--- src/main/java/turing/mods/polaris/util/Lists.java
+++ src/main/java/turing/mods/polaris/util/Lists.java
@@ -1,5 +1,6 @@
 package turing.mods.polaris.util;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Tuple;
 
 import java.util.*;
@@ -15,5 +16,18 @@ public class Lists {
         Map<K, V> map = new HashMap<>();
         Arrays.stream(values).forEach(tuple -> map.put(tuple.getA(), tuple.getB()));
         return map;
+    }
+
+    public static <T> boolean compareLists(List<T> list1, List<T> list2) {
+        if (list1 == null || list2 == null) return false;
+        return list1.containsAll(list2);
+    }
+
+    public static boolean chancedOutputsEqual(List<Tuple<ItemStack, Integer>> list1, List<ItemStack> list2) {
+        if (list1 == null || list2 == null) return false;
+        for (Tuple<ItemStack, Integer> tuple : list1) {
+            if (!list2.contains(tuple.getA())) return false;
+        }
+        return true;
     }
 }
