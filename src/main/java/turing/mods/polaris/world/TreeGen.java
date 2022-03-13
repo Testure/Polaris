fc7ee76dbb9ca7f54ea0d59cf70420e347f0e55f
Turing6 <39919441+Testure@users.noreply.github.com>
2022-03-12 21:17:57 -0600

    Add more rubber tree generators

--- src/main/java/turing/mods/polaris/world/TreeGen.java
+++ src/main/java/turing/mods/polaris/world/TreeGen.java
@@ -1,7 +1,9 @@
 package turing.mods.polaris.world;
 
 import com.google.common.collect.ImmutableList;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.Features;
 import net.minecraft.world.gen.feature.MultipleRandomFeatureConfig;
@@ -9,13 +11,23 @@ import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import turing.mods.polaris.registry.FeatureRegistry;
+import turing.mods.polaris.util.MathUtil;
 
 public class TreeGen {
+    public static final ConfiguredFeature<?, ?> RUBBER_TREE = FeatureRegistry.RUBBER.get().withConfiguration(RubberTree.DEFAULT_CONFIG);
+    public static final ConfiguredFeature<?, ?> RUBBER_TREE_TALL = FeatureRegistry.RUBBER.get().withConfiguration(RubberTree.TALL_CONFIG);
+    public static final ConfiguredFeature<?, ?> RUBBER_WATER = Feature.RANDOM_SELECTOR.withConfiguration(new MultipleRandomFeatureConfig(ImmutableList.of(RUBBER_TREE_TALL.withChance(0.2F)), RUBBER_TREE)).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(0, 1.0F, 1)));
+    public static final ConfiguredFeature<?, ?> RUBBER_MOUNTAIN = Feature.RANDOM_SELECTOR.withConfiguration(new MultipleRandomFeatureConfig(ImmutableList.of(RUBBER_TREE_TALL.withChance(0.1F)), RUBBER_TREE)).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(0, 0.1F, 1)));
+    public static final ConfiguredFeature<?, ?> RUBBER_BIRCH = Feature.RANDOM_SELECTOR.withConfiguration(new MultipleRandomFeatureConfig(ImmutableList.of(RUBBER_TREE_TALL.withChance(0.1F)), RUBBER_TREE)).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(10, 0.1F, 1)));
+
     public static void genTrees(BiomeLoadingEvent event) {
-        event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.RANDOM_SELECTOR
-                .withConfiguration(new MultipleRandomFeatureConfig(ImmutableList.of(FeatureRegistry.RUBBER.get().withConfiguration(RubberTree.TALL_CONFIG).withChance(0.2F)), FeatureRegistry.RUBBER.get().withConfiguration(RubberTree.DEFAULT_CONFIG)))
-                .withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
-                .withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(0, 0.1F, 1)))
-        );
+        generateRubberTrees(event);
+    }
+
+    private static void generateRubberTrees(BiomeLoadingEvent event) {
+        if (event.getCategory() == Biome.Category.PLAINS) event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, RUBBER_WATER);
+        if (MathUtil.isWithinBoundsF(event.getClimate().temperature, 0.4F, 1.0F)) event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, RUBBER_MOUNTAIN);
+        if (event.getCategory() == Biome.Category.FOREST) event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, RUBBER_BIRCH);
+        if (event.getCategory() == Biome.Category.EXTREME_HILLS) event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, RUBBER_MOUNTAIN);
     }
 }
